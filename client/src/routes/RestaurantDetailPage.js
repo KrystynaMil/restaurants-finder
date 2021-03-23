e6db894aecb6c38618898bab806216c727543e36
KrystynaMil <kristina-manager@hotmail.com>
2021-03-23 11:24:53 +0100

    created routes/ Home.js , RestaurantDetailPage.js and UpdatePage.js updated App.js

---
+++ client/src/routes/RestaurantDetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const RestaurantDetailPage = () => {
+    return (
+        <div>
+           Detail Page
+        </div>
+    )
+}
+
+export default RestaurantDetailPage;
